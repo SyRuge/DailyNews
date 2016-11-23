c3f5c3294c9213723763cbcba6f8e321c5a388d1
Xuanyuan <xyjk1993@163.com>
2016-11-23 11:46:17 +0800

    First commit

---
+++ app/src/main/java/com/xcx/dailynews/Constants.java
@@ -0,0 +1,12 @@
+package com.xcx.dailynews;
+
+/**
+ * Created by xcx on 2016/11/23.
+ */
+
+public class Constants {
+    public static final int COMMON_TYPE=1;
+    public static final int TOP_REFRESH=2;
+    public static final int BOTTOM_REFRESH=3;
+    public static final int PHOTO_TYPE=4;
+}
